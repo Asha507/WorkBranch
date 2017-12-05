9ef4ac70c2bb222cbe5f1d8c403ba6446dfe364d
unknown <asha.polamarasetty@gmail.com>
2017-12-05 23:38:57 +0530

    Home page UX

--- Hello/Controllers/authenticate-controller.js
+++ Hello/Controllers/authenticate-controller.js
@@ -11,9 +11,9 @@ MongoClient.connect("mongodb://localhost:27017/Valuemomentum", function(err, db)
     }
 });
 module.exports.authenticate=function(req,res){
-    var email=req.body.email;
+    var userID=req.body.UserID;
     var password=req.body.password;
-    var query={name:email};
+    var query={name:userID};
     dbObject.collection("Login").find(query).toArray(function(err, result) {
       if (err) {
         res.json({
