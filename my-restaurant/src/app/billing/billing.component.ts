98d44270dec6cacfa47014968bf8e38b27bb721a
unknown <asha.polamarasetty@gmail.com>
2018-01-29 23:11:06 +0530

    'TablesScreen

--- my-restaurant/src/app/billing/billing.component.ts
+++ my-restaurant/src/app/billing/billing.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BillingComponent implements OnInit {
 
-  constructor() { }
-
+  Numbers:any;
+    constructor() {
+      this.Numbers = Array(20).fill(0).map((x,i)=>i);
+    }
+ 
   ngOnInit() {
   }
 
