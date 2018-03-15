509b30d798fd2ee499a5a690d93aa6c521f57f9e
Asha <asha.polamarasetty@gmail.com>
2018-03-15 08:29:55 +0530

    Blockchain changes carshop

--- Blockchain/Blockchain/src/app/app.module.ts
+++ Blockchain/Blockchain/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import{RouterModule,Routes} from '@angular/router'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -27,6 +28,7 @@ const routes:Routes=[
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(routes, {useHash: true}),
   ],
   providers: [],
--- Blockchain/Blockchain/src/app/car-shop/car-shop.component.ts
+++ Blockchain/Blockchain/src/app/car-shop/car-shop.component.ts
@@ -6,10 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-shop.component.css']
 })
 export class CarShopComponent implements OnInit {
-
+  costS: string = "";
+  costSD: string = "";
+  sNumber: number = 0;
+  sdNumber: number = 0;
   constructor() { }
 
   ngOnInit() {
   }
+  Cost(event) {
+    debugger;
+    this.sNumber = this.GenerateRandomNumber();
+    switch (event.target.selectedIndex) {
+      case 1:
+        this.costS = "6L"
+        break;
+      case 2:
+        this.costS = "8L"
+        break;
+      case 3:
+        this.costS = "10L"
+        break;
+      default:
+        this.costS = "";
+        break;
+    }
 
+  }
+
+  GenerateRandomNumber() {
+    return Math.floor(Math.random() * (50 - 1 + 1)) + 1;
+  }
+  CostSD(event) {
+    debugger;
+    this.sdNumber = this.GenerateRandomNumber();
+    switch (event.target.selectedIndex) {
+      case 1:
+        this.costSD = "6L"
+        break;
+      case 2:
+        this.costSD = "8L"
+        break;
+      case 3:
+        this.costSD = "10L"
+        break;
+      default:
+        this.costSD = "";
+        break;
+    }
+  }
 }
